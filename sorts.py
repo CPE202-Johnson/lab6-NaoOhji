25db9bc86a6a89c039befbb6f121f853904f8221
lab0-NaoOhji <nmuroga@calpoly.edu>
2021-02-23 22:20:09 -0800

    first try

--- sorts.py
+++ sorts.py
@@ -2,10 +2,35 @@ import random
 import time
 
 def selection_sort(list):
-    pass
-    
+    comps = 0
+    for index in range(len(list)):
+        minIndex = index
+        j = index+1
+        while j<len(list):
+            if list[j] < list[minIndex]:
+                minIndex = j
+            j += 1
+            comps += 1
+        swap(list[index],list[minIndex])
+    return comps
+
+def swap(x,y):
+    temp = x
+    x = y
+    y = temp
+
 def insertion_sort(list):
-    pass
+    comps = 0
+    for index in range(len(list)):
+        j = index
+        value = index
+        while value != -1 and list[j] <= list[value]:
+            value -= 1
+            comps += 1
+        if value +1 != j:
+            temp = list.pop(j)
+            list.insert(value+1,temp)
+    return comps
    
 
 def main():
@@ -15,9 +40,10 @@ def main():
     random.seed(1234) 
     
     # Generate 5000 random numbers from 0 to 999,999
-    randoms = random.sample(range(1000000), 5000)
+    randoms = random.sample(range(1000000), 32000)
     start_time = time.time() 
-    comps = selection_sort(randoms)
+    #comps = selection_sort(randoms)
+    comps = insertion_sort(randoms)
     stop_time = time.time()
     print(comps, stop_time - start_time)
 
--- sorts_tests.py
+++ sorts_tests.py
@@ -1,11 +1,13 @@
 import unittest
 from sorts import *
+from quicksort import *
 
 class TestLab4(unittest.TestCase):
 
     def test_simple(self):
         nums = [23, 10]
         comps = selection_sort(nums)
+        #comps = quick_sort(nums)
         self.assertEqual(comps, 1)
         self.assertEqual(nums, [10, 23])
 
